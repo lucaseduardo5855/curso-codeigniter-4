ad22a8d29070fa1dce837b70c17294e57a5111a5
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-09-14 21:30:50 -0300

    feat: validação de usuario e logout e restaurante puxando via session, ajuste de layoult

--- app/Views/partials/main_menu.php
+++ app/Views/partials/main_menu.php
@@ -1,19 +1,17 @@
-<aside class="main-menu" style="width: 250px; padding: 20px 0; background: #330000;">
+<aside class="main-menu">
+  <p class="menu-group mb-3"><?= session()->user['restaurant_name'] ?></p>
   <nav class="navbar-nav">
-    <a href="<?= base_url() ?>" class="nav-link px-3 py-2" style="color: #fff; text-decoration: none; display: block;">
+    <a href="<?= site_url('/') ?>" class="nav-link">
       <i class="fas fa-home"></i> Início
     </a>
-    <a href="#" class="nav-link px-3 py-2" style="color: #fff; text-decoration: none; display: block;">
-      <i class="fas fa-users"></i> Usuários
+    <a href="#" class="nav-link">
+      <i class="fas fa-burger"></i> Produtos
     </a>
-    <a href="#" class="nav-link px-3 py-2" style="color: #fff; text-decoration: none; display: block;">
-      <i class="fas fa-utensils"></i> Produtos
+    <a href="#" class="nav-link">
+      <i class="fas fa-layer-group"></i> Stock Disponível
     </a>
-    <a href="#" class="nav-link px-3 py-2" style="color: #fff; text-decoration: none; display: block;">
-      <i class="fas fa-chart-bar"></i> Relatórios
-    </a>
-    <a href="#" class="nav-link px-3 py-2" style="color: #fff; text-decoration: none; display: block;">
-      <i class="fas fa-cog"></i> Configurações
+    <a href="#" class="nav-link">
+      <i class="fas fa-chart-column"></i> Dados Estatísticos
     </a>
   </nav>
 </aside>
