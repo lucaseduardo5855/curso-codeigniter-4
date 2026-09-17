38a2ec6c279a7ea98c802d77b355d9b17078b311
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-09-17 20:41:02 -0300

    feat: mostrando funcionalidade de uploads

--- projeto_19_upload/app/Controllers/Home.php
+++ projeto_19_upload/app/Controllers/Home.php
@@ -18,10 +18,43 @@ class Home extends BaseController
         // print_r($this->request->getFile('file_upload'));
         
         //verificar se o envio foi feito com sucesso
-        if ($this->request->getFile('file_upload')->isValid()) {
-            echo 'ok';
-        } else {
-            echo 'nok';
-        }
+        // if ($this->request->getFile('file_upload')->isValid()) {
+        //     echo 'ok';
+        // } else {
+        //     echo 'nok';
+        // }
+
+        //atribuição de dados de um ficheiro a uma variavel
+       $ficheiro = $this->request->getFile('file_upload');
+        // if ($ficheiro->isValid()) {
+        //     echo 'ok';
+        // } else {
+        //     echo 'nok';
+        // }
+
+        // buscar dados de um ficheiro
+        echo '<br>';
+        echo $ficheiro->getClientName();
+        //ou
+        echo '<br>';
+        echo $ficheiro->getName();
+
+        echo '<br>';
+        echo $ficheiro->getTempName();
+
+        echo '<br>';
+        echo $ficheiro->getExtension();
+
+        echo '<br>';
+        echo $ficheiro->getMimeType();
+
+        echo '<br>';
+        echo $ficheiro->getSize();
+
+        echo '<br>';
+        echo $ficheiro->getSizeByUnit('kb') . 'kb';
+
+        echo '<br>';
+        echo $ficheiro->getSizeByUnit('mb') . 'mb';
     }
 }
